131fce5f6c936cae6493b811f799ffadc7451c19
chimzoe2002-a11y <chimzoe2002@gmail.com>
2026-08-31 09:13:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -351,7 +351,7 @@ async function procesarMensajeWhatsApp(body) {
 
   await enviarMensajeWhatsApp(numeroCliente, respuesta);
 }
-});
+
 
 
 // ======================================================
